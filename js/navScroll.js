dd44927ba05478e2a4c3b631f8ccb12864028e65
M <faymatt96@gmail.com>
2019-07-22 21:46:18 -0400

    Remove comment

--- js/navScroll.js
+++ js/navScroll.js
@@ -1,13 +1,3 @@
-/****************************************************************
-** This script serves the responsive menu for mobile browsers. **
-** May decide to delete this, and use a different solution.    **
-** Also, this script is untested.                              **
-**  							       **
-** GPL license, of course. 				       **
-** - Matt                                                      **
-** April 19th, 2018					       **
-*****************************************************************/
-
 $(document).ready(() => {
   // Adds class sticky to menu on page scroll
   var navScrollTop = $('nav').offset().top;
